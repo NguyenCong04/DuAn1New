799e6b24b271710f5c464518d0f90f19eeeeee79
Nguyễn Đức Duy <nguyenduyhaihauc@gmail.com>
2023-11-23 20:31:10 +0700

    duy update 20h30

---
+++ app/src/main/java/congntph34559/fpoly/duan1newapplication/Fragment/FragmentDanhSachDonDatAdmin.java
@@ -0,0 +1,39 @@
+package congntph34559.fpoly.duan1newapplication.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import congntph34559.fpoly.duan1newapplication.Adapter.AdapterDanhSachDonDatAdmin;
+import congntph34559.fpoly.duan1newapplication.DTO.DanhSachDonDatAdminDTO;
+import congntph34559.fpoly.duan1newapplication.R;
+
+public class FragmentDanhSachDonDatAdmin extends Fragment {
+    private RecyclerView recyclerViewDsDonDat;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frg_danh_sach_don_admin, container, false);
+        recyclerViewDsDonDat = view.findViewById(R.id.recyclerDanhSachHoaDonAdmin);
+
+        AdapterDanhSachDonDatAdmin adapterDanhSachDonDatAdmin = new AdapterDanhSachDonDatAdmin(getDSHD());
+        recyclerViewDsDonDat.setAdapter(adapterDanhSachDonDatAdmin);
+        return view;
+    }
+
+    private List<DanhSachDonDatAdminDTO> getDSHD() {
+        List<DanhSachDonDatAdminDTO> list = new ArrayList<>();
+        list.add(new DanhSachDonDatAdminDTO(R.drawable.img_bong_cai_trang_trang_chu, "Nguyễn Văn Công", "Bông cai trắng", "1kg", "17.000 VND"));
+        list.add(new DanhSachDonDatAdminDTO(R.drawable.img_bong_cai_trang_trang_chu, "Nguyễn Văn Công", "Bông cai trắng", "1kg", "17.000 VND"));
+        return list;
+    }
+}
---
+++ app/src/main/java/congntph34559/fpoly/duan1newapplication/Fragment/FragmentThongKeAdmin.java
@@ -0,0 +1,40 @@
+package congntph34559.fpoly.duan1newapplication.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import congntph34559.fpoly.duan1newapplication.Adapter.AdapterThongKeAdmin;
+import congntph34559.fpoly.duan1newapplication.DTO.ThongKeAdmin;
+import congntph34559.fpoly.duan1newapplication.R;
+
+public class FragmentThongKeAdmin extends Fragment {
+    private RecyclerView recyclerViewThongKe;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frg_thong_ke_admin, container, false);
+        recyclerViewThongKe = view.findViewById(R.id.recyclerThongKe);
+        AdapterThongKeAdmin adapterThongKeAdmin = new AdapterThongKeAdmin(getThongKeAdmin());
+        recyclerViewThongKe.setAdapter(adapterThongKeAdmin);
+        return view;
+    }
+
+    private List<ThongKeAdmin> getThongKeAdmin() {
+        List<ThongKeAdmin> list = new ArrayList<>();
+        list.add(new ThongKeAdmin(R.drawable.img_cai_thao_trang_chu, "Tên sản phẩm; Cải Thảo", "Số lượn mua: 50 lượt"));
+        list.add(new ThongKeAdmin(R.drawable.img_bong_cai_xanh_trang_chu, "Tên sản phẩm; Bông cai xanh", "Số lượn mua: 50 lượt"));
+        list.add(new ThongKeAdmin(R.drawable.img_bong_cai_trang_trang_chu, "Tên sản phẩm; Bông cải trắng", "Số lượn mua: 50 lượt"));
+
+        return list;
+    }
+}
